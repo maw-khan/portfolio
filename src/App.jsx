4768ae054210e38f943101500cda01dfa99366a3
Muhammad Ali Waris Khan <MAWKHAN5031@GMAIL.COM>
2026-09-18 20:19:36 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,7 @@ const projects = [
     desc: "End-to-end Python ETL pipeline integrating PostgreSQL with transformed book, customer, and order data for analytics and visualization.",
     github: "https://github.com/maw-khan/booknook-books-data-pipeline",
     image: "/projects/booknook/Summary.png",
-    architecture: "/projects/booknook/Summary.png",
+    architecture: "/projects/booknook/Summry.png",
 
     gallery: [
       "/projects/booknook/6_summary.png",
